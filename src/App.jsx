e9aa73ea348401ad1e119bd8b21817c496e7fe62
Michał Lisowiec <miclisowiec@gmail.com>
2021-12-07 21:07:57 +0100

    Add header with Fox component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import Fox from './components/Fox';
 import './styles/custom.scss';
 
 function App() {
 
   return (
-    <div>Hello world!</div>
+    <header className="bg-primary text-secondary text-center svg-border-rounded vh-100">
+      <Fox />
+    </header>
   );
 }
 
---
+++ src/components/Fox.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import fox from '../images/fox.webp';
+import '../components/Fox';
+
+function Fox() {
+
+  return (        
+    <div className="container d-flex align-items-center flex-column justify-content-center min-vh-100">
+      <div className="container-fluid">
+        <img className="img-fluid" src={fox} alt="fox"/>
+      </div>
+      <h1 className="masthead-heading">Michał Lisowiec</h1>
+      <h2 className="masthead-subheading">Junior Front-end Developer</h2>
+    </div>
+  );
+}
+
+export default Fox;
