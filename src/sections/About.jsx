469155f2ed667f32ee04a79b93fc6719bc117288
Michał Lisowiec <miclisowiec@gmail.com>
2021-12-18 19:18:49 +0100

    Make the photo swing

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState, useLayoutEffect } from 'react';
+import { useWindowScrollPosition } from '../hooks/useWindowScrollPosition';
 import about from '../images/about.webp';
 
 function About() {
+
+  const scrollPosition = useWindowScrollPosition();
+  const [swinging, setSwinging] = useState(false);
+  useLayoutEffect(() => {
+    if(scrollPosition > 199){
+      setSwinging(true);
+    }
+  });
 
   return (        
     <section id="about" className="py-4 text-center bg-light">
@@ -9,7 +18,7 @@ function About() {
         <h3 className="mb-4">O mnie</h3>
         <div className="row gx-0 mb-4 mb-lg-5 align-items-center">
           <div className="col-xl-8 col-lg-7">
-            <img className="img-fluid mb-3 mb-lg-0 w-75" src={about} alt="Me in the Alps"/>
+            <img className={(swinging ? 'swinging' : '') + ' img-fluid mb-3 mb-lg-0 w-75'} src={about} alt="Me in the Alps"/>
           </div>
           <div className="col-xl-4 col-lg-5">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
