196ec236647145199f44bbc7189f5f04de26e4da
Michał Lisowiec <miclisowiec@gmail.com>
2021-12-19 20:21:29 +0100

    Add hover effect to the projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { projects } from '../data/projects';
 import uuid from 'react-uuid';
+import '../styles/custom.scss';
 
 function Projects() {
 
@@ -8,9 +9,15 @@ function Projects() {
     return(
       <div key={uuid()} className="container px-4 text-secondary">
         <div className="row gx-0 mb-4 mb-lg-5 align-items-center">
-          <div className={`col-lg-6 ${i % 2 !== 0 ? 'order-lg-2' : ''}`}>
+          <figure className={`figure col-lg-6 hover-img bg-black overflow-hidden position-relative rounded-3 ${i % 2 !== 0 ? 'order-lg-2' : ''}`}>
             <img className="img-fluid" src={project.image} alt={`project ${project.title}`} />
-          </div>
+            <figcaption className="d-flex justify-content-center align-items-center position-absolute top-0 bottom-0 start-0 end-0">
+              {project.title === 'Portfolio'
+                ? ''
+                : <button type="button" className="btn btn-primary px-5 mx-3">Strona</button>}
+              <button type="button" className="btn btn-primary px-5 mx-3">Github</button>
+            </figcaption>
+          </figure>
           <div className="col-lg-6">
             <div className="project-text w-100 mt-2 mb-4 px-4 text-center text-lg-left">
               <h4 className="text-light">{project.title}</h4>
