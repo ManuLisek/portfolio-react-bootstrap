4e1e5b99c70954c3078bcfc9c0bc52defe76b435
Michał Lisowiec <miclisowiec@gmail.com>
2022-01-02 12:21:23 +0100

    Move skills outside of Technologies component

--- src/sections/Technologies.jsx
+++ src/sections/Technologies.jsx
@@ -4,24 +4,25 @@ import { technologies } from '../data/technologies';
 import ScrollAnimation from 'react-animate-on-scroll';
 import uuid from 'react-uuid';
 
+const skills = technologies.map(skill => {
+  return(
+    <div key={uuid()} className="col-lg-3 col-md-4 col-sm-6 mb-5 w-sm-50">
+      <div className="card bg-light border-0 h-100">
+        <div className="card-body p-4 p-lg-5 pt-0 pt-lg-0">
+          <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4"><i className="bi bi-bootstrap"></i></div>
+          <h4 className="h4">{skill.technology}</h4>
+          <ScrollAnimation animateIn='flipInY' animateOnce={true}>
+            <skill.image className={skill.color}/>
+          </ScrollAnimation>
+        </div>
+      </div>
+    </div>
+  );
+});
+
 function Technologies() {
 
   const [lang] = useContext(LangContext);
-  const skills = technologies.map(skill => {
-    return(
-      <div key={uuid()} className="col-lg-3 col-md-4 col-sm-6 mb-5 w-sm-50">
-        <div className="card bg-light border-0 h-100">
-          <div className="card-body p-4 p-lg-5 pt-0 pt-lg-0">
-            <div className="feature bg-primary bg-gradient text-white rounded-3 mb-4"><i className="bi bi-bootstrap"></i></div>
-            <h4 className="h4">{skill.technology}</h4>
-            <ScrollAnimation animateIn='flipInY' animateOnce={true}>
-              <skill.image className={skill.color}/>
-            </ScrollAnimation>
-          </div>
-        </div>
-      </div>
-    );
-  });
 
   return (        
     <section id="technologies" className="py-4 bg-secondary text-center">
